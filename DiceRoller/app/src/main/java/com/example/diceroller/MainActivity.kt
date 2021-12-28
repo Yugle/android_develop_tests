74f065b4a04039517ed28587530de7601a736492
phil.zhu@dhms.com.cn <phil.zhu@dhms.com.cn>
2021-12-28 10:14:30 +0800

    add readme

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        rollDice() // 初始显示随机骰子数
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "Rolled!", Toast.LENGTH_SHORT)
@@ -24,8 +24,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val result = this.dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = result.toString()
+        // val resultTextView: TextView = findViewById(R.id.textView)
+        // resultTextView.text = result.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = result.toString()
     }
 }
 
